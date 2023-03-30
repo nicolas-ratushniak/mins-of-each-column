4d4cf3dd48b3b79ba8ee9b42549cd142ae61310d
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2023-03-30 17:35:15 +0300

    Add MartixCalculator

---
+++ MatrixLib/MatrixCalculator.cs
@@ -0,0 +1,24 @@
+﻿namespace MatrixLib;
+
+public static class MatrixCalculator
+{
+    public static double[] GetMinsOfEachColomn(in double[,] matrix)
+    {
+        var result = new double[matrix.GetLength(1)];
+
+        for (int col = 0;  col < matrix.GetLength(1); col++)
+        {
+            double minInRow = matrix[0, col];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                if (matrix[row, col] < minInRow)
+                {
+                    minInRow = matrix[row, col];
+                }
+            }
+            result[col] = minInRow;
+        }
+        return result;
+    }
+}
