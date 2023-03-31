f7836fca27d5ccea6a51bfaaeec2c52dc266dff6
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2023-03-31 22:33:31 +0300

    Fix a bug in matrix decoder

--- MatrixLib/MatrixDecoder.cs
+++ MatrixLib/MatrixDecoder.cs
@@ -14,14 +14,14 @@ public class MatrixDecoder
             throw new ArgumentNullException(nameof(matrix));
         }
 
-        StringBuilder sb = new StringBuilder();
+        StringBuilder sb = new();
 
         for (int i = 0; i < matrix.GetLength(0); i++)
         {
             for (int j = 0; j < matrix.GetLength(1); j++)
             {
                 sb.Append(matrix[i, j]);
-                sb.Append(j < matrix.GetLength(2) - 1 ? _separator : _newLine);
+                sb.Append(j < matrix.GetLength(1) - 1 ? _separator : _newLine);
             }
         }
         return sb.ToString();
