578c1dc29b2de5f74576ada392eb368ec93d5615
Mykola Ratushniak <nicolas.ratushniak@gmail.com>
2023-03-31 19:11:53 +0300

    Add property for matrix input string

--- WPFapp/MainWindow.xaml.cs
+++ WPFapp/MainWindow.xaml.cs
@@ -1,28 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using MatrixLib;
 
 namespace WpfApp
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private string _matrixInput = "";
+
+        public string MatrixInput
+        {
+            get => _matrixInput;
+
+            set
+            {
+                if (value != _matrixInput)
+                {
+                    _matrixInput = value;
+                }
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
     }
 }
